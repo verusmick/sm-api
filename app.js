5e1f22da752ec9e12167112c54babd194cbcf7fe
Micky <miguel.angel.angulo72@gmail.com>
2018-05-14 18:52:14 -0400

    Configure cors.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./api/routes/index');
 var usersRouter = require('./api/routes/users');
 var clientsRouter = require('./api/routes/clients');
+var cors = require('cors')
 
 var app = express();
 
@@ -14,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
