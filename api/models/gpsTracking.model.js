d67858e18e8779b64fd247cb2fcf710f3e80f1f3
Micky <miguel.angel.angulo72@gmail.com>
2018-09-19 16:46:45 -0400

    Save coordinates in db

--- api/models/gpsTracking.model.js
+++ api/models/gpsTracking.model.js
@@ -1,15 +1,29 @@
 // const sanaMedicDB = require('../Database/sanaMedicDB')
 const express = require('express');
 const app = express();
+const sanaMedicDB = require('../Database/sanaMedicDB')
 
-exports.postSellersTrack = function (req, res) {
-  let coordinate = req.body[0];
+exports.postSellersTrack = function (req, res, next) {
+  let coordinate = req.body;
+// console.log('coordinate')
+// console.log(req)
+
   app.emit('coordinate', {
     latitude: coordinate.latitude,
     longitude: coordinate.longitude,
     timestamp: new Date()
   });
-  res.json({status: "success", message: "coordinate added successfully!!!", data: null});
+
+  let ts= parseInt(new Date());
+  console.log(new Date())
+
+  let query = `INSERT INTO coordinates (id_user, latitude, longitude, timestamp) VALUES ('7029468LP','${coordinate.latitude}','${coordinate.longitude}','${new Date(new Date()).getTime()}')`;
+  console.log('query')
+  console.log(query)
+  sanaMedicDB.query(query, (err, results) => {
+    if (err) throw next(err);
+    res.json({status: "success", message: "coordinate added successfully!!!", data: null});
+  })
 }
 
 exports.sellersTrackEvents = (req, res, next) => {
