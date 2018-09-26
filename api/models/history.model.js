51d40219fa5d73c1dd67afe0e275598cb93e4572
Micky <miguel.angel.angulo72@gmail.com>
2018-09-26 13:34:14 -0400

    Sellers History > Added dinamic variables since and until in this endpoint.

--- api/models/history.model.js
+++ api/models/history.model.js
@@ -1,7 +1,11 @@
 const sanaMedicDB = require('../Database/sanaMedicDB')
+const moment = require('moment');
 
 exports.getSellerHistoryPerDay = (req, res) => {
-  sanaMedicDB.query('SELECT * FROM coordinates', function (err, results) {
+  let since = req.query.since;
+  let until = req.query.until;
+  let query = `SELECT * FROM coordinates WHERE timestamp BETWEEN '${since} 00:00:01' AND '${until} 23:59:59'`;
+  sanaMedicDB.query(query, function (err, results) {
     if (err) throw err
     return res.json(results)
   })
