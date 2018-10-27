8d74cf15dfa32f215ee3dca2fe17004b0432a4f0
Micky <miguel.angel.angulo72@gmail.com>
2018-10-26 23:42:57 -0400

    Added getTrackStatus EndPoint.

--- api/models/history.model.js
+++ api/models/history.model.js
@@ -4,7 +4,8 @@ const moment = require('moment');
 exports.getSellerHistoryPerDay = (req, res) => {
   let since = req.query.since;
   let until = req.query.until;
-  let query = `SELECT * FROM coordinates WHERE timestamp BETWEEN '${since} 00:00:01' AND '${until} 23:59:59'`;
+  let userId = req.query.userId;
+  let query = `SELECT * FROM coordinates WHERE user_id='${userId}' AND timestamp BETWEEN '${since} 00:00:01' AND '${until} 23:59:59'`;
   sanaMedicDB.query(query, function (err, results) {
     if (err) throw err
     return res.json(results)
