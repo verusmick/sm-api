c6988572eb099a2fd368de9ac68d3944b7d7b539
Micky <miguel.angel.angulo72@gmail.com>
2018-10-31 15:56:59 -0400

    Added improvements in GET clients.

--- api/models/clients.model.js
+++ api/models/clients.model.js
@@ -1,7 +1,10 @@
 const sicivDB = require('../Database/sicivDB')
 
 exports.getClients = (req, res) => {
-  sicivDB.query('SELECT * FROM clientes', function (err, results) {
+  let like = req.query.like;
+  let likeQuery = like ? ` WHERE razon_social LIKE '%${like}%'` : '';
+  let query = `SELECT * FROM clientes` + likeQuery;
+  sicivDB.query(query, function (err, results) {
     if (err) throw err
     return res.json(results)
   })
