908dcd4ca9398ee2ca69f824f1c76b913f9acbeb
Micky <miguel.angel.angulo72@gmail.com>
2018-11-14 17:24:30 -0400

    Added order create functionality.

--- api/models/order.model.js
+++ api/models/order.model.js
@@ -2,6 +2,9 @@
  * Created by Micky on 11/12/2018.
  */
 const sanaMedicDB = require('../Database/sanaMedicDB')
+const sicivDB = require('../Database/sicivDB')
+var _ = require('lodash');
+const moment = require('moment');
 
 exports.getAll = (req, res, next) => {
   // let query = `SELECT roles.role_id AS roleId, roles.name, roles.role_code AS codeRole FROM roles`;
@@ -13,4 +16,111 @@ exports.getAll = (req, res, next) => {
   //     res.json({status: "success", message: "Roles list found!!!", data: {}});
   //   }
   // })
+}
+
+exports.create = (req, res, next) => {
+  let body = req.body, userId = req.headers.userid;
+  createOrRefreshUser(body).then(_ => {
+    let query = `INSERT INTO 
+    orders (    
+    client_cache_id, 
+    user_id, 
+    order_date, 
+    delivery_date, 
+    pay_type_id, 
+    total, 
+    nit, 
+    bill_name) VALUES (
+    '${body.clientId}',
+     '${userId}', 
+    '${moment().format('YYYY-MM-DD HH:mm:ss')}', 
+    NULL, 
+    '${body.payType}', 
+    '${body.total}', '${body.nit}','${body.billName}')`;
+    sanaMedicDB.query(query, (err, results) => {
+      if (err) throw next(err);
+      res.json({status: "success", message: "Order added successfully!!!", data: null});
+    })
+  })
+}
+
+function createOrRefreshUser(userData) {
+  return new Promise((resolve, reject) => {
+    getCacheClient(userData.clientId).then(result => {
+      if (result.length === 0) {
+        createCacheClient(userData).then(_ => {
+          resolve();
+        });
+      } else {
+        resolve();
+      }
+    })
+  })
+}
+
+// function updateCacheClient(userData) {
+//   let queryBill = userData.billName ? `bill_name = '${userData.billName}'` : '';
+//   let queryNit = userData.nit? ` ${queryBill?',':''}nit = '${userData.nit}'`:'';
+//   return new Promise((resolve, reject) => {
+//     if(!queryBill || !queryNit){
+//       resolve();
+//     }
+//     let query = `UPDATE cache_clients
+//     SET
+//     ${queryBill}
+//     ${queryNit}
+//     WHERE cache_clients.id_cliente = ${userData.clientId}`;
+//     sanaMedicDB.query(query, (err, results) => {
+//       if (err) {
+//         reject(err)
+//       } else {
+//         resolve(results);
+//       }
+//     })
+//   });
+// }
+
+function createCacheClient(userData) {
+  return new Promise((resolve, reject) => {
+    let query = `INSERT INTO cache_clients (
+    id_cliente, 
+    address,
+    latitude,
+    longitude,
+    nit,
+    bill_name)
+    VALUES ('${userData.clientId}', NULL, NULL, NULL, NULL, NULL)`;
+    sanaMedicDB.query(query, (err, results) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(results);
+      }
+    })
+  });
+}
+
+function getCacheClient(clientId) {
+  return new Promise((resolve, reject) => {
+    let query = `SELECT * FROM cache_clients WHERE id_cliente = '${clientId}'`;
+    sanaMedicDB.query(query, (err, results) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(results);
+      }
+    })
+  });
+}
+
+exports.getById = (req, res, next) => {
+
+}
+
+exports.deleteById = (req, res, next) => {
+
+}
+
+exports.updateById = (req, res, next) => {
+
 }
--- api/routes/order.js
+++ api/routes/order.js
@@ -4,5 +4,9 @@ var orderHandlers = require('../models/order.model.js');
 
 /* GET users listing. */
 router.get('/', orderHandlers.getAll);
+router.post('/', orderHandlers.create);
+router.get('/:orderId', orderHandlers.getById);
+router.delete('/:orderId', orderHandlers.deleteById);
+router.put('/:orderId', orderHandlers.updateById);
 
 module.exports = router;
