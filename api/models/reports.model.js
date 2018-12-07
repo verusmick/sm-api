9748b550a0b960cdac0c8499abedc2d947dd62f0
Micky <miguel.angel.angulo72@gmail.com>
2018-12-07 01:36:15 -0400

    Added report endpoint

--- api/models/reports.model.js
+++ api/models/reports.model.js
@@ -1,5 +1,6 @@
 const sanaMedicDB = require('../Database/sanaMedicDB')
-
+const sicivDB = require('../Database/sicivDB')
+var _ = require('lodash');
 exports.getStatusGpsPerSeller = (req, res, next) => {
   let since = req.query.since;
   let until = req.query.until;
@@ -36,5 +37,50 @@ exports.bestSellers = (req, res, next) => {
     } else {
       res.json({status: "success", message: "Report list found!!!", data: results});
     }
+  })
+}
+
+exports.getOrders = (req, res, next) => {
+  let since = req.query.since;
+  let until = req.query.until;
+  let query = `SELECT 
+  orders.order_id AS orderId, 
+  orders.client_cache_id AS clientId, 
+  orders.user_id AS userId,
+  orders.order_date AS orderDate,
+  orders.registered_date AS registeredDate,
+  orders.pay_type_id AS payType,
+  orders.total AS total,
+  orders.nit AS nit,
+  orders.bill_name AS billName
+  FROM orders WHERE order_date BETWEEN '${since} 00:00:01' AND '${until} 23:59:59' ORDER BY orders.order_date ASC`;
+  sanaMedicDB.query(query, function (err, results) {
+    if (err) {
+      next(err)
+    } else {
+      let clientPromises = [];
+      results.forEach(product => {
+        clientPromises.push(getClientById(product.clientId))
+      });
+      Promise.all(clientPromises).then(clientsList => {
+        results.forEach((product, index) => {
+          product['client'] = clientsList[index].length > 0 ? clientsList[index][0] : {};
+        });
+        res.json({status: "success", message: "Report list found!!!", data: results});
+      })
+    }
+  });
+}
+
+function getClientById(clientId) {
+  return new Promise((resolve, reject) => {
+    let query = `SELECT * FROM clientes WHERE id_cliente = '${clientId}'`;
+    sicivDB.query(query, function (err, results) {
+      if (err)
+        reject(err);
+      else {
+        resolve(results)
+      }
+    })
   })
 }
--- api/routes/reports.js
+++ api/routes/reports.js
@@ -5,5 +5,6 @@ var reportsHandlers = require('../models/reports.model.js');
 /* GET users listing. */
 router.get('/getStatusGpsPerSeller', reportsHandlers.getStatusGpsPerSeller);
 router.get('/bestSellers', reportsHandlers.bestSellers);
+router.get('/orders', reportsHandlers.getOrders);
 
 module.exports = router;
