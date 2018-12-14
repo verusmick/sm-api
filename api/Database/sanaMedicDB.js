852b7b0d19f09f8c3ed703c0c5ab0802d25e5c79
Micky <miguel.angel.angulo72@gmail.com>
2018-12-14 11:10:52 -0400

    Change params

--- api/Database/sanaMedicDB.js
+++ api/Database/sanaMedicDB.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'control123!',
+  password: '',
   database: 'sanamedic_db'
 
 })
