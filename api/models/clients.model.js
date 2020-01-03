6ebd7eb559c4f8ff334f034bb3f979581ece25f0
Micky <miguel.angel.angulo72@gmail.com>
2020-01-02 21:29:46 -0400

    Update clients endpoint to add debt Client param

--- api/models/clients.model.js
+++ api/models/clients.model.js
@@ -2,9 +2,12 @@ const sicivDB = require('../Database/sicivDB')
 const logs = require('../logs/logs.service')
 
 exports.getClients = (req, res) => {
+  console.log('Aca === >');
   let like = req.query.like;
-  let likeQuery = like ? ` WHERE razon_social LIKE '%${like}%'` : '';
-  let query = `SELECT * FROM clientes` + likeQuery;
+  let likeQuery = like ? ` razon_social LIKE '%${like}%'` : '';
+  let debt = req.query.debt ? likeQuery ? ` AND deuda_actual ${req.query.debt}` : ` deuda_actual ${req.query.debt}`: '';
+  let where = debt || likeQuery ? ' WHERE' : '';
+  let query = `SELECT * FROM clientes` + where + likeQuery + debt;
   sicivDB.query(query, function (err, results) {
     if (err){
       logs.write(req.headers.username, req.headers.ci, 'Error en obtencion de lista de Clientes del sistema SICIV.');
